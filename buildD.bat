7dd181cc433d6a1143dd082c7b6905ea05bce5fe
Jonathan Whiting <jonathan@ignika.com>
2011-12-17 13:06:57 +0000

    Debug builder

---
+++ buildD.bat
@@ -0,0 +1,2 @@
+mxmlc -optimize=true -output main.swf -static-link-runtime-shared-libraries=true -compatibility-version=3.0.0 --target-player=10.0.0 -compiler.debug=true Preloader.as -frames.frame mainframe Main
+
